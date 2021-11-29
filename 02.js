0e235beedb9422d91f92999578525103a50bc10e
Simone Mazzola <s.mazzola@donboscosandona.it>
2021-11-29 11:56:11 +0100

    9uij9i

---
+++ 02.js
@@ -0,0 +1,34 @@
+// Scrivere un ptogramma che , dato il nomwe di una persona, 
+// la saluti con una frase
+
+// SOLUZIONE
+// definisco una variabile nome
+let nome;
+
+// le assegno come valore una stringa con un nome a scelta
+nome = "Andrea";
+
+// definisco una variabile frase
+let frase;
+
+// compongo la frase inserendo anche il nome
+frase = "Buongiorno " + nome + ", è un piacere averti qui.";
+
+// scrivo nella coinsole la frase
+console.log(frase);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
