f8dbc12607e3a9e0b55d94420e5e335dbaf829a3
Simone Mazzola <s.mazzola@donboscosandona.it>
2021-11-29 12:14:27 +0100

    oiloy

---
+++ 04.js
@@ -0,0 +1,17 @@
+// scrivere un programma che , dato il nome di una persona
+// e un numero intero
+// scriva il nome di quella persona tante volte quanto
+// è specificato dal numero
+
+// SOLUZIONE
+// definisco e assegno  una variabiòle nome
+let nome = " Paolo ";
+
+// definisco e assegno una variabile numero
+let numero = 5,
+
+// scrivo un ciclo for che conti da 1 a numero
+for(i01; i<=numero; i++) {
+
+}
+//       ad ogni ripetizione scrivo il nome
