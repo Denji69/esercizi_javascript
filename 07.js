4d773e8ba4dd8da776b6c9b521bfc267aad8bcfa
Simone Mazzola <s.mazzola@donboscosandona.it>
2021-11-29 13:15:20 +0100

    eswdrhjuiyd5rfejuidr4eyhnjuihytg jutygrfdehnjumhhnjbvgfcdxrfvg

---
+++ 07.js
@@ -0,0 +1,10 @@
+// scrivere un programma che dati
+// il prezzo del biglietto
+let prezzo = 4.5;
+// e il numero di persone
+let persone = 150;
+// calcoli il totale da pagare
+let costo_totale
+costo_totale = prezzo * persone ;
+// e lo scriva in una frase
+console.log("Il prezzo da pagare è di " + costo_totale + " €");
---
+++ 08.js
@@ -0,0 +1,26 @@
+// scrivere un programma che dati
+// il prezzo del biglietto
+let prezzo_minori = 4.5;
+let prezzo_maggiori = 6.5;
+// l'età delle persone
+let maggiorenni 
+let minorenni
+
+// e il numero di persone
+let persone = 150;
+
+// calcoli il totale da pagare
+let costo_totale
+costo_totale = prezzo * persone ;
+
+//definisco l'età 
+let anni = 12;
+
+// e lo scriva in una frase
+
+
+console.log("Il prezzo da pagare è di " + costo_totale + " €");
+
+// nota: Bisognerà definire due prezzi, uno per i minorenni
+
+// e uno per i maggiorenni
